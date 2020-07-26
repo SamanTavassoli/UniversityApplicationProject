96ffa17d2b5fc755d8acf38b827401e38622eff4
SamanTavassoli <samandevelopper1@gmail.com>
2020-07-26 19:04:00 +0200

    Initial commit

---
+++ src/main/java/com/thesamas/universityapplicationproject/UniversityApplicationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.thesamas.universityapplicationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityApplicationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityApplicationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thesamas/universityapplicationproject/UniversityApplicationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesamas.universityapplicationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApplicationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
