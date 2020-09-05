c22b7dd344a2357b2d3882e1544fa9d243468d83
SamanTavassoli <samandevelopper1@gmail.com>
2020-09-05 13:41:19 +0200

    Create basic login page
    toggle for login / register
    toggle for uni / student

--- front end/src/app/user-authentication/user-authentication.component.ts
+++ front end/src/app/user-authentication/user-authentication.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { UserAuthenticationService } from '../_services/user-authentication.service';
 
+enum AuthentificationType {
+  Login,
+  Register,
+}
+
+enum UserType {
+  Student,
+  University,
+}
+
 @Component({
   selector: 'app-user-authentication',
   templateUrl: './user-authentication.component.html',
@@ -8,19 +18,30 @@ import { UserAuthenticationService } from '../_services/user-authentication.serv
 })
 export class UserAuthenticationComponent implements OnInit {
 
+  AuthentificationType = AuthentificationType;
+  authentificationType: AuthentificationType = AuthentificationType.Login;
+
+  UserType = UserType;
+  userType: UserType = UserType.Student;
+
+
   constructor(private Auth: UserAuthenticationService) { }
 
   ngOnInit(): void {
 
   }
 
-  loginTest(event) {
+  login(event) {
     const target = event.target
     const username = target.querySelector('#username').value
     const password = target.querySelector('#password').value
 
     this.Auth.login(username, password);
     
+  }
+
+  register(event) {
+
   }
 
 }
